e0c5fc9b003facdb78cc05a94fe80a06a97d69ee
NadezhdaSakal <sakaln88@gmail.com>
2025-02-24 15:54:04 +0300

    task 2

--- app/src/test/java/ru/yandex/praktikumchatapp/ChatViewModelTest.kt
+++ app/src/test/java/ru/yandex/praktikumchatapp/ChatViewModelTest.kt
@@ -1,3 +1,4 @@
+import app.cash.turbine.test
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
@@ -5,6 +6,8 @@ import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -32,7 +35,11 @@ class ChatViewModelTest {
     @Test
     fun `send message should update messages with MyMessage`() = runTest {
         val message = Message.MyMessage("TestMessage")
-
+        viewModel.sendMyMessage("TestMessage")
+        viewModel.messages.test {
+            val actual = awaitItem().find { it == message }
+            assertThat(actual, equalTo(message))
+        }
     }
 
     @Test
