4fdfef20c824cc8ffb0ab939f4856759acc11779
NadezhdaSakal <sakaln88@gmail.com>
2025-02-24 18:56:01 +0300

    fix after review

--- app/src/main/java/ru/yandex/praktikumchatapp/data/ChatRepository.kt
+++ app/src/main/java/ru/yandex/praktikumchatapp/data/ChatRepository.kt
@@ -3,20 +3,25 @@ package ru.yandex.praktikumchatapp.data
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.retryWhen
+import java.util.concurrent.atomic.AtomicLong
 
 class ChatRepository(
     private val api: ChatApi = ChatApi()
 ) {
-    private var currentDelay = 200L
+    private var currentDelay = AtomicLong(200L)
 
     fun getReplyMessage(): Flow<String> {
+        val maxRetries = 3
         return api.getReply().retryWhen { cause, attempt ->
+            if (attempt >= maxRetries) {
+                return@retryWhen false
+            }
             if (cause is Exception) {
                 if (attempt % 3 == 0L) {
-                    currentDelay = 200L
+                    currentDelay.set(200L)
                 }
-                delay(currentDelay)
-                currentDelay *= DELAY_FACTOR
+                delay(currentDelay.get())
+                currentDelay.set(currentDelay.get() * DELAY_FACTOR)
             }
             true
         }
