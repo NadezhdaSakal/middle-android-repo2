b0b7c66c569e380e2e0d33020f9d66e1f9c3766e
NadezhdaSakal <sakaln88@gmail.com>
2025-02-24 16:20:30 +0300

    task 3

--- app/src/test/java/ru/yandex/praktikumchatapp/ChatViewModelTest.kt
+++ app/src/test/java/ru/yandex/praktikumchatapp/ChatViewModelTest.kt
@@ -1,6 +1,10 @@
 import app.cash.turbine.test
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.resetMain
@@ -45,6 +49,18 @@ class ChatViewModelTest {
     @Test
     fun testReceiveMessage_concurrentMessages() = runTest {
         val messagesToSend = (1..100).map { Message.MyMessage("Message $it") }
-
+        val jobs = mutableListOf<Job>()
+        messagesToSend.forEach { myMessage ->
+            val job = launch {
+                viewModel.sendMyMessage(myMessage.text)
+                viewModel.messages.test {
+                    assertThat(myMessage, equalTo(awaitItem().find { it == myMessage }))
+                }
+            }
+            jobs.add(job)
+        }
+        jobs.joinAll()
+        val actual = viewModel.messages.first().toList()
+        assertThat(actual, equalTo(messagesToSend))
     }
 }
